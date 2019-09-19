1525f927e7f0f57f23fbcd3b2ee97981645e29c6
Vlad GURDIGA <gurdiga@gmail.com>
2019-09-19 10:04:58 +0300

    <Form/>: actionButtons

--- src/Common/Components/Form.css.ts
+++ src/Common/Components/Form.css.ts
@@ -1,8 +1,10 @@
 import {style} from "typestyle";
-import {verticallySpaced, vertical} from "csstips";
+import {verticallySpaced, vertical, horizontal} from "csstips";
 import {em} from "csx";
 
 export namespace FormCss {
-  export const FieldList = style(verticallySpaced(em(0.5)));
-  export const FieldContainer = style(vertical);
+  export const FormContainer = style(vertical, verticallySpaced(em(1)));
+  export const FieldListContainer = style(verticallySpaced(em(0.5)));
+  export const FieldContainer = style(vertical, verticallySpaced(em(0.1)));
+  export const ActionButtonListContainer = style(horizontal);
 }
--- src/Common/Components/Form.tsx
+++ src/Common/Components/Form.tsx
@@ -4,15 +4,21 @@ import {FormCss} from "./Form.css";
 
 interface Props {
   fields: JSX.Element[];
+  actionButtons: JSX.Element[];
 }
 
 export const Form = (props: Props) => (
-  <form>
-    <ul className={FormCss.FieldList}>
+  <form onSubmit={e => e.preventDefault()} className={FormCss.FormContainer}>
+    <ul className={FormCss.FieldListContainer}>
       {props.fields.map((field, i) => (
         <li key={i} className={FormCss.FieldContainer}>
           {field}
         </li>
+      ))}
+    </ul>
+    <ul className={FormCss.ActionButtonListContainer}>
+      {props.actionButtons.map((actionButton, i) => (
+        <li key={i}>{actionButton}</li>
       ))}
     </ul>
   </form>
