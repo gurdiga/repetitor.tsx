67c9c64ffe11dbb16a2ec5527ee9f807df735672
Vlad GURDIGA <gurdiga@gmail.com>
2019-09-30 11:07:31 +0300

    PasswordField: add <EyeButton/>

--- src/Common/Components/FormFields/PasswordField.css.ts
+++ src/Common/Components/FormFields/PasswordField.css.ts
@@ -6,4 +6,16 @@ export namespace PasswordFieldCss {
     fontSize: percent(80),
     paddingLeft: em(0.25),
   });
+
+  const eyeSize = em(1.5);
+
+  export const EyeButton = style({
+    border: "dashed 1px green",
+    background: "transparent",
+    cursor: "pointer",
+    width: eyeSize,
+    height: eyeSize,
+    padding: 0,
+    position: "absolute",
+  });
 }
--- src/Common/Components/FormFields/PasswordField.tsx
+++ src/Common/Components/FormFields/PasswordField.tsx
@@ -8,10 +8,6 @@ import {FormValidation} from "Common/FormValidation";
 
 interface Props extends FormField.CommonProps {}
 
-const validationRules: FormValidation.ValidationRules = {
-  "Can’t be empty": value => !!value,
-};
-
 export const PasswordField = (props: Props) => {
   const {id, label, value, onValueChange, showValidationMessage} = props;
 
@@ -28,6 +24,7 @@ export const PasswordField = (props: Props) => {
         validationRules={validationRules}
         showValidationMessage={showValidationMessage}
       />
+      <button className={PasswordFieldCss.EyeButton}>{eye}</button>
       <Checkbox
         id={`${id}-show`}
         checked={shouldUnmaskPassword}
@@ -38,3 +35,18 @@ export const PasswordField = (props: Props) => {
     </>
   );
 };
+
+const validationRules: FormValidation.ValidationRules = {
+  "Can’t be empty": value => !!value,
+};
+
+const eye = (
+  <img
+    src={`data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -18.982 100 100" overflow="visible">
+ <path d="M49 62.035c-26.167 0-46.674-27.814-47.535-28.999L0 31.018l1.465-2.019C2.326 27.814 22.833 0 49 0c26.166 0 46.674 27.814 47.534 28.999L98 31.018l-1.466 2.019C95.674 34.221 75.166 62.035 49 62.035zM8.616 31.014C13.669 37.151 30.117 55.166 49 55.166c18.928 0 35.339-18.006 40.386-24.145C84.331 24.883 67.885 6.869 49 6.869c-18.928 0-35.339 18.005-40.384 24.145z"/>
+ <circle cx="49" cy="31.018" r="18.997"/>
+</svg>`}
+    alt="Demască parola"
+    title="Demască parola"
+  />
+);
