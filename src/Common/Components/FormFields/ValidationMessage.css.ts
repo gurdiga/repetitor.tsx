bea2837424a55923a71ca1e120e4208ae2f718da
Vlad GURDIGA <gurdiga@gmail.com>
2019-10-01 18:35:43 +0300

    <ValidationMessage/>:  red

--- src/Common/Components/FormFields/ValidationMessage.css.ts
+++ src/Common/Components/FormFields/ValidationMessage.css.ts
@@ -1,8 +1,14 @@
 import {style} from "typestyle";
-import {percent} from "csx";
+import {percent, px, quote} from "csx";
 
 export namespace ValidationMessageCss {
   export const Text = style({
+    color: "red",
     fontSize: percent(80),
+    $nest: {
+      "&::before": {
+        content: quote("↑"),
+      },
+    },
   });
 }
