9b265c9c385487d05753f37857c094d86df9698d
Vlad GURDIGA <gurdiga@gmail.com>
2019-10-02 08:19:29 +0300

    FormValidation.validateWithRules: fix bug

--- src/Common/FormValidation.ts
+++ src/Common/FormValidation.ts
@@ -29,7 +29,7 @@ export namespace FormValidation {
         validationMessage = message;
         return false;
       } else {
-        validationMessage = message;
+        validationMessage = "";
         return true;
       }
     });
