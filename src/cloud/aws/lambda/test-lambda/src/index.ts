9102043227af8b649bbf5ca77b45e7f050c8949a
Vlad GURDIGA <gurdiga@gmail.com>
2019-12-03 09:05:08 +0200

    cloud: lambda: print env

--- src/cloud/aws/lambda/test-lambda/src/index.ts
+++ src/cloud/aws/lambda/test-lambda/src/index.ts
@@ -1,11 +1,11 @@
-import * as MySQL from "mysql";
+// import * as MySQL from "mysql";
 import {APIGatewayProxyEvent} from "aws-lambda";
 
 exports.handler = async (event: APIGatewayProxyEvent) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify({"MySQL exports": Object.keys(MySQL)}),
+    body: JSON.stringify({ENV: process.env}),
   };
 
   return response;
