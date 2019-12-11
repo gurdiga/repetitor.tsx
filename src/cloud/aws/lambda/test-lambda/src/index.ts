65e639d2d2d48912b893c8a66e40496a2806932c
Vlad GURDIGA <gurdiga@gmail.com>
2019-12-11 10:46:05 +0200

    Lambda: can run Aurora queries

--- src/cloud/aws/lambda/test-lambda/src/index.ts
+++ src/cloud/aws/lambda/test-lambda/src/index.ts
@@ -1,12 +1,44 @@
-// import * as MySQL from "mysql";
-import {APIGatewayProxyEvent} from "aws-lambda";
+import {Handler, APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import * as MySQL from "mysql";
 
-exports.handler = async (event: APIGatewayProxyEvent) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ENV: process.env}),
-  };
+const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event, context) => {
+  return new Promise<APIGatewayProxyResult>((resolve, reject) => {
+    console.time("MySQL connection");
 
-  return response;
+    var connection = MySQL.createConnection({
+      host: process.env.APP_DB_HOST,
+      user: process.env.APP_DB_USER,
+      password: process.env.APP_DB_PASSWORD,
+      database: process.env.APP_DB_NAME,
+    });
+
+    connection.connect();
+    console.timeEnd("MySQL connection");
+
+    console.time("MySQL query");
+    connection.query({sql: "SELECT 1 + 1 AS solution", values: [], timeout: 20000}, function(error, results, fields) {
+      console.timeEnd("MySQL query");
+
+      if (error) {
+        console.error(error);
+        reject(error);
+        return;
+      }
+
+      const solution = results[0].solution;
+
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify({APP_RESULT: solution}),
+      });
+
+      console.log("The solution is:", solution);
+    });
+
+    connection.end();
+  }).catch(e => {
+    throw e;
+  });
 };
+
+exports.handler = handler;
