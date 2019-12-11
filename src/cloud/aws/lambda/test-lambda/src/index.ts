ef6de023c24d35d5eee17891110f2cdf9d733824
Vlad GURDIGA <gurdiga@gmail.com>
2019-12-11 10:54:18 +0200

    Lambda: cache the DB connection in the execution context

--- src/cloud/aws/lambda/test-lambda/src/index.ts
+++ src/cloud/aws/lambda/test-lambda/src/index.ts
@@ -1,19 +1,23 @@
 import {Handler, APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import * as MySQL from "mysql";
 
+let connection: MySQL.Connection;
+
 const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event, context) => {
   return new Promise<APIGatewayProxyResult>((resolve, reject) => {
-    console.time("MySQL connection");
+    if (!connection) {
+      console.time("MySQL connection");
 
-    var connection = MySQL.createConnection({
-      host: process.env.APP_DB_HOST,
-      user: process.env.APP_DB_USER,
-      password: process.env.APP_DB_PASSWORD,
-      database: process.env.APP_DB_NAME,
-    });
+      connection = MySQL.createConnection({
+        host: process.env.APP_DB_HOST,
+        user: process.env.APP_DB_USER,
+        password: process.env.APP_DB_PASSWORD,
+        database: process.env.APP_DB_NAME,
+      });
 
-    connection.connect();
-    console.timeEnd("MySQL connection");
+      connection.connect();
+      console.timeEnd("MySQL connection");
+    }
 
     console.time("MySQL query");
     connection.query({sql: "SELECT 1 + 1 AS solution", values: [], timeout: 20000}, function(error, results, fields) {
@@ -25,17 +29,11 @@ const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (eve
         return;
       }
 
-      const solution = results[0].solution;
-
       resolve({
         statusCode: 200,
-        body: JSON.stringify({APP_RESULT: solution}),
+        body: JSON.stringify({APP_RESULT: results[0].solution}),
       });
-
-      console.log("The solution is:", solution);
     });
-
-    connection.end();
   }).catch(e => {
     throw e;
   });
