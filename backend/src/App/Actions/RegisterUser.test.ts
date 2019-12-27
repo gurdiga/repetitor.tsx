98f59fc5700a0e5b04bdf2826f4232ce6f91ce1b
Vlad GURDIGA <gurdiga@gmail.com>
2019-12-27 20:30:54 +0200

    backend: a mock test

---
+++ backend/src/App/Actions/RegisterUser.test.ts
@@ -0,0 +1,31 @@
+import "mocha";
+import {expect} from "chai";
+import {RegisterUser} from "App/Actions/RegisterUser";
+// import {runQuery} from "App/DB";
+
+describe("RegisterUser", () => {
+  describe("assertValidParams", () => {
+    it("works", () => {
+      const params = {};
+
+      expect(() => {
+        RegisterUser.assertValidParams(params);
+      }).not.to.throw;
+    });
+  });
+
+  describe("execute", () => {
+    // before(() => {});
+
+    // after(() =>
+    //   runQuery({
+    //     sql: "DELETE FROM users",
+    //     params: [],
+    //   })
+    // );
+
+    it("runs", () => {
+      expect(RegisterUser.execute).to.exist;
+    });
+  });
+});
