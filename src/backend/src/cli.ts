34e889bd45dfad1ba85dcb571116fcd453ec2654
Vlad GURDIGA <gurdiga@gmail.com>
2019-12-27 14:38:43 +0200

    backend: cli: housekeeping

--- src/backend/src/cli.ts
+++ src/backend/src/cli.ts
@@ -1,11 +1,6 @@
-import * as assert from "assert";
 import {handleActionRequest} from "App/Backend";
 
-const cliArgument = process.argv[2];
-
-assert(!!cliArgument, "Please pass in actionName and actionParams as JSON as the first argument.");
-
-const request = JSON.parse(cliArgument);
+const request = JSON.parse(process.argv[2]);
 const {actionName, actionParams = {}} = request;
 
 handleActionRequest({actionName, actionParams})
