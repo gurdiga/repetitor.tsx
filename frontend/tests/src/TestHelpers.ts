d0e8c8013b16d6421820c1bcc2e8de99847ca237
Vlad GURDIGA <gurdiga@gmail.com>
2020-01-16 11:13:07 +0200

    frontend/tests: add chai-enzyme

--- frontend/tests/src/TestHelpers.ts
+++ frontend/tests/src/TestHelpers.ts
@@ -1,4 +1,7 @@
 import {configure} from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
+import * as chai from "chai";
+import * as chaiEnzyme from "chai-enzyme";
 
 configure({adapter: new Adapter()});
+chai.use(chaiEnzyme());
