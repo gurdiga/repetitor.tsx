2b860fe49ee0bbb4b8fa57f3d03ab537d79551cc
Vlad GURDIGA <gurdiga@gmail.com>
2020-02-11 11:06:15 +0200

    Housekeeping

--- backend/src/ScenarioHandlers/UserRegistration.ts
+++ backend/src/ScenarioHandlers/UserRegistration.ts
@@ -1,17 +1,12 @@
-import debug from "debug";
-import {genRandomString, hashString, getStorablePassword} from "../Utils/StringUtils";
+import {getStorablePassword} from "../Utils/StringUtils";
 import {createUser} from "../Persistence/UserPersistence";
 import {makeUserFromRegistrationFormDTO} from "shared/Model/User";
 import {ScenarioRegistry} from "shared/ScenarioRegistry";
-
-const log = debug("app:UserRegistration");
 
 type Scenario = ScenarioRegistry["UserRegistration"];
 
 export async function UserRegistration(dto: Scenario["DTO"]): Promise<Scenario["Result"]> {
   const result = makeUserFromRegistrationFormDTO(dto);
-
-  log({dto, result});
 
   if (result.kind !== "User") {
     return result;
--- backend/src/Utils/Env.ts
+++ backend/src/Utils/Env.ts
@@ -1,11 +1,5 @@
 import * as assert from "assert";
 
-export function assertEnvVars(varNames: string[]) {
-  const missingVars = varNames.filter(name => !(name in process.env)).join(", ");
-
-  assert(missingVars.length === 0, `Env vars are missing: ${missingVars}`);
-}
-
 export function requireEnvVar(varName: string): string {
   assert(varName in process.env, `Env var is missing: ${varName}`);
 
