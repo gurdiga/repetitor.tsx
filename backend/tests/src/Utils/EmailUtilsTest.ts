6d0a8a3851aeec0d1bb698cc6723bf042c276d18
Vlad GURDIGA <gurdiga@gmail.com>
2020-03-04 08:44:53 +0200

    EmailUtils: tweak unit test
    Increase the timeout.

--- backend/tests/src/Utils/EmailUtilsTest.ts
+++ backend/tests/src/Utils/EmailUtilsTest.ts
@@ -1,7 +1,8 @@
 import {transporter} from "Utils/EmailUtils";
 
 describe("EmailUtils", () => {
-  it("can connect", () => {
+  it("can connect", function() {
+    this.timeout(5000);
     return transporter.verify();
   });
 });
