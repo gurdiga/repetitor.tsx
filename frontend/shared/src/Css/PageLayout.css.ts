11baa0582529d060e1c7cb05abe98b8585b3486d
Vlad GURDIGA <gurdiga@gmail.com>
2020-03-09 10:28:07 +0200

    PageLayout: maxWidth of 35em

--- frontend/shared/src/Css/PageLayout.css.ts
+++ frontend/shared/src/Css/PageLayout.css.ts
@@ -1,4 +1,4 @@
-import {center, horizontal, padding, vertical, margin} from "csstips";
+import {center, horizontal, padding, vertical, margin, maxWidth} from "csstips";
 import {em} from "csx";
 import {GlobalCss} from "frontend/shared/Css/Global.css";
 import {style} from "typestyle";
@@ -6,8 +6,13 @@ import {style} from "typestyle";
 GlobalCss.setupGlobalStyles();
 
 export namespace PageLayoutCss {
-  export const Wrapper = style(vertical, center, padding(em(0.5), em(1)));
-  export const Title = style(padding(em(0.5), 0, em(1), 0), margin(0));
-  export const MainContent = style(padding(em(1), 0, em(1), 0), {});
-  export const FooterWrapper = style(horizontal, padding(em(1)));
+  export const Wrapper = style(vertical, center, {
+    $debugName: "Wrapper",
+    ...maxWidth(em(35)),
+    ...margin(0, "auto"),
+    ...padding(em(0.5), em(1)),
+  });
+  export const Title = style({$debugName: "Title", textAlign: "center"}, padding(em(0.5), 0, em(1), 0), margin(0));
+  export const MainContent = style({$debugName: "MainContent"}, padding(em(1), 0, em(1), 0), {});
+  export const FooterWrapper = style({$debugName: "FooterWrapper"}, horizontal, padding(em(1)));
 }
