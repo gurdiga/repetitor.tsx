4468a66b2365d31cd482779eed0ac340b1c1d004
Vlad GURDIGA <gurdiga@gmail.com>
2020-03-09 10:58:39 +0200

    CheckboxCss: fix vertical alignment

--- frontend/shared/src/Components/FormFields/Checkbox.css.ts
+++ frontend/shared/src/Components/FormFields/Checkbox.css.ts
@@ -3,5 +3,8 @@ import {em} from "csx";
 import {style} from "typestyle";
 
 export namespace CheckboxCss {
-  export const Container = style(horizontal, horizontallySpaced(em(0.25)), {marginTop: em(0.5)});
+  export const Container = style({$debugName: "Container"}, horizontal, horizontallySpaced(em(0.25)), {
+    marginTop: em(0.5),
+    alignItems: "baseline",
+  });
 }
