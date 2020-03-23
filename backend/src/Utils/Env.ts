4abb119fde0c4d3e8e5562597a169873f3ec86b2
Vlad GURDIGA <gurdiga@gmail.com>
2020-03-23 22:04:30 +0200

    Type-check env var names

--- backend/src/Utils/Env.ts
+++ backend/src/Utils/Env.ts
@@ -1,12 +1,31 @@
 import * as assert from "assert";
 
-export function requireEnvVar(varName: string): string {
+type EnvVarName =
+  | "NODE_ENV"
+  | "HEROKU_SLUG_COMMIT"
+  | "APP_NAME"
+  | "APP_URL"
+  | "PORT"
+  | "APP_DB_HOST"
+  | "APP_DB_USER"
+  | "APP_DB_PASSWORD"
+  | "APP_DB_NAME"
+  | "APP_SESSION_COOKIE_SECRET"
+  | "APP_SMTP_HOST"
+  | "APP_SMTP_PORT"
+  | "APP_SMTP_USER"
+  | "APP_SMTP_PASSWORD"
+  | "APP_EMAIL_CONFIRMATION_MESSAGE_SENDER_ADDRESS"
+  | "APP_ROLLBAR_POST_CLIENT_ITEM_TOKEN"
+  | "APP_ROLLBAR_POST_SERVER_ITEM_TOKEN";
+
+export function requireEnvVar(varName: EnvVarName): string {
   assert(varName in process.env, `Env var is missing: ${varName}`);
 
   return process.env[varName]!;
 }
 
-export function requireNumericEnvVar(varName: string): number {
+export function requireNumericEnvVar(varName: EnvVarName): number {
   const n = parseInt(requireEnvVar(varName), 10);
 
   assert(!isNaN(n), `Expected env var ${varName} to be a number, not ${n}`);
