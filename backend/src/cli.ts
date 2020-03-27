21f11a9c3f6037c1b80372ad7b3aa8d36cd9e1c9
Vlad GURDIGA <gurdiga@gmail.com>
2020-03-27 22:44:34 +0200

    Houskeeping

--- backend/src/cli.ts
+++
@@ -1,9 +0,0 @@
-import {runScenario} from "backend/src/Utils/ScenarioRunner";
-import {connectionPool as DbConnectionPool} from "backend/src/Utils/Db";
-
-const {scenarioioName, scenarioInput} = JSON.parse(process.argv[2]);
-
-runScenario(scenarioioName, scenarioInput)
-  .then(console.log)
-  .catch(console.error)
-  .finally(DbConnectionPool.end);
