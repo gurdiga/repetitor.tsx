72de89eee36eff8cb31ce3e7d10c40522a918fd2
Vlad GURDIGA <gurdiga@gmail.com>
2020-04-06 21:06:16 +0300

    <Label/>: Don’t add the undefined class

--- frontend/shared/src/Components/FormFields/Label.tsx
+++ frontend/shared/src/Components/FormFields/Label.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import {LabelCss} from "frontend/shared/src/Components/FormFields/Label.css";
+import {classList} from "frontend/shared/src/Utils/ClassList";
 
 interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {}
 
 export function Label(props: Props) {
   const {children, className: customClassName, ...rest} = props;
-  const className = `${LabelCss.Label} ${customClassName}`;
+  const className = classList(LabelCss.Label, customClassName);
 
   return (
     <label className={className} {...rest}>
