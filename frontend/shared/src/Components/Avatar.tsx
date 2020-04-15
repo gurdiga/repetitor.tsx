d1b40a583868f5b7a8fbe67b4b15761db8f2d465
Vlad GURDIGA <gurdiga@gmail.com>
2020-04-15 10:11:14 +0300

    Introduce <Avatar/>

---
+++ frontend/shared/src/Components/Avatar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {Link} from "shared/src/Model/Profile";
+
+interface Props {
+  url: Link | undefined; // When undefined render a default avatar or a spinner
+}
+
+export function Avatar(_props: Props) {
+  return <pre className="avatar">TODO: Add avatar.</pre>;
+}
